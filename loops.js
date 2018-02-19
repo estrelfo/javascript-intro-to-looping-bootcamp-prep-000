76460651031c33161444dd89ed4f90e561204533
bruno refos <brunesrefuso2@gmail.com>
2018-02-19 14:32:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -25,11 +25,13 @@ function whileLoop (number)
 
 function doWhileLoop (array)
 {
-  
+  do {
+    
+  } while (maybeTrue(array));
 }
 
 function maybeTrue() {
-  return Math.random() >= 0.5
+  return Math.random() >= 0.5;
 }
 while (maybeTrue()) {
   console.log("And I ran; I ran so far away!");
